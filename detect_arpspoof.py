5bdc3add76c1c2179551e8ba8d689d1bc0fbd05c
Uvemode <uvemode@icloud.com>
2018-01-03 16:28:02 +0100

    Update detect_arpspoof.py

--- detect_arpspoof.py
+++ detect_arpspoof.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import os, time, netifaces, sys, logging
+import subprocess as s
+import simpleaudio as sa
 from sys import platform
 from scapy.all import sniff
 
@@ -8,20 +10,15 @@ request_threshold = 10
 
 # The script requires root to run. Check if user is root
 if os.geteuid() != 0:
-	exit("Root permisson is required to operate on network interfaces. \nNow Aborting.")
+    exit("Root permisson is required to operate on network interfaces. \nNow Aborting.")
 
-# Prompt to select log file
-filename = raw_input("Please input desired log file name. [spoof.log]")
-# If none specified, set to default
-if filename == "":
-    filename = "spoof.log"
+filename = "" # Your log file
 
 # Set logging structure
 logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=filename, filemode="a", level=logging.DEBUG)
 # Read available network interfaces
 available_interfaces = netifaces.interfaces()
-# Ask user for desired interface
-interface = raw_input("Please select the interface you wish to use. {}\n".format(str(available_interfaces)))
+interface = "" # Your interface
 # Check if specified interface is valid
 if not interface in available_interfaces:
     exit("Interface {} not available.".format(interface))
@@ -37,7 +34,7 @@ requests = []
 replies_count = {}
 notification_issued = []
 
-logging.info("ARP Spoofing Detection Started on {}".format(local_ip))
+logging.info("ARP Poisoning Detection Started on {}".format(local_ip))
 
 def check_spoof (source, mac, destination):
     # Function checks if a specific ARP reply is part of an ARP spoof attack or not
@@ -55,9 +52,9 @@ def check_spoof (source, mac, destination):
 
         if (replies_count[mac] > request_threshold) and (not mac in notification_issued):
             # Check number of replies reaches threshold or not, and whether or not we have sent a notification for this MAC addr
-            logging.error("ARP Spoofing Detected from MAC Address {}".format(mac)) # Logs the attack in the log file
+            logging.error("ARP Poisoning Detected from MAC Address {}".format(mac)) # Logs the attack in the log file
             # Issue OS Notification
-            issue_os_notification("ARP Spoofing Detected", "The current network is being attacked.", "ARP Spoofing Attack Detected from {}.".format(mac))
+            issue_os_notification("ARP Poisoning Detected", "Attack from {}.".format(mac))
             # Add to sent list to prevent repeated notifications.
             notification_issued.append(mac)
     else:
@@ -75,23 +72,10 @@ def packet_filter (packet):
     if operation == 'is-at':
         return check_spoof (source, source_mac, dest)
 
-def issue_os_notification(title, subtitle, content):
-    # Helper function that issues OS notification
-    # Currently only Mac OS X is implemented
-    if platform == "darwin":
-        import AppKit
-        # Create instance of OS X notification center
-        notification_center = AppKit.NSUserNotificationCenter.defaultUserNotificationCenter()
-        # Create new notification instance
-        notification = AppKit.NSUserNotification.alloc().init()
-        notification.setTitle_(title)
-        notification.setSubtitle_(subtitle)
-        notification.setInformativeText_(content)
-        # Deliver OS notifications
-        notification_center.deliverNotification_(notification)
-    else:
-        logging.info("Tried to issue OS notification, but it is not yet supported. ")
+def issue_os_notification(title, content):
+    s.call(['notify-send',title,content])
 
-print("ARP Spoofing Detection Started. Any output is redirected to log file.")
+print("ARP Poisoning Detection Started. Any output is redirected to log file.")
 # Rely on scapy sniff function to do the hard job - sniffing packets.
 sniff(filter = "arp", prn = packet_filter, store = 0)
+
